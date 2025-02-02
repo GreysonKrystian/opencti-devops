8f0aefa6f43ff05c88fad21414e97410198b68bc
Krystian Grela <krystian12340@gmail.com>
2025-02-02 22:35:58 +0100

    testing terraform

---
+++ infra/terraform/networking.tf
@@ -0,0 +1,25 @@
+resource "azurerm_virtual_network" "kg-opencti-vnet-test" {
+  name                = "kg-opencti-vnet-test"
+  address_space       = ["10.0.0.0/20"]
+  location            = azurerm_resource_group.kg-opencti-test.location
+  resource_group_name = azurerm_resource_group.kg-opencti-test.name
+}
+
+resource "azurerm_subnet" "opencti-subnet-test" {
+  name                 = "opencti-subnet-test"
+  resource_group_name  = azurerm_resource_group.kg-opencti-test.name
+  virtual_network_name = azurerm_virtual_network.kg-opencti-vnet-test.name
+  address_prefixes     = ["10.0.241.0/24"]
+}
+
+resource "azurerm_private_dns_zone" "opencti-private-dns-test" {
+  name                = "privatelink.redis.cache.windows.net"
+  resource_group_name = azurerm_resource_group.kg-opencti-test.name
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "opencti-private-virtual-network-dns-test" {
+  name                  = "opencti-private-virtual-network-dns-test"
+  resource_group_name   = azurerm_resource_group.kg-opencti-test.name
+  private_dns_zone_name = azurerm_private_dns_zone.opencti-private-dns-test.name
+  virtual_network_id    = azurerm_virtual_network.kg-opencti-vnet-test.id
+}
---
+++ infra/terraform/storage.tf
@@ -0,0 +1,36 @@
+resource "azurerm_redis_cache" "opencti-redis-cache-test" {
+    name                = "opencti-redis-cache-test"
+    location            = azurerm_resource_group.kg-opencti-test.location
+    resource_group_name = azurerm_resource_group.kg-opencti-test.name
+    capacity            = 0
+    family              = "C"
+    sku_name            = "Basic"
+    minimum_tls_version = "1.2"
+
+    redis_configuration {
+        maxmemory_policy = "allkeys-lru"
+    }
+    public_network_access_enabled = false
+}
+
+resource "azurerm_private_endpoint" "opencti-redis-cache-test-pe" {
+  name                = "opencti-redis-cache-test-pe"
+  location            = azurerm_resource_group.kg-opencti-test.location
+  resource_group_name = azurerm_resource_group.kg-opencti-test.name
+  subnet_id           = azurerm_subnet.opencti-subnet-test.id
+
+  private_service_connection {
+    name                           = "example-privateserviceconnection"
+    private_connection_resource_id = azurerm_redis_cache.opencti-redis-cache-test.id
+    is_manual_connection           = false
+    subresource_names              = ["redisCache"]
+  }
+}
+
+resource "azurerm_private_dns_a_record" "opencti-redis-cache-test-dns-record" {
+  name                = "opencti-redis-cache-test-dns-record"
+  zone_name           = azurerm_private_dns_zone.opencti-private-dns-test.name
+  resource_group_name = azurerm_resource_group.kg-opencti-test.name
+  ttl                 = 300
+  records             = [azurerm_private_endpoint.opencti-redis-cache-test-pe.private_service_connection[0].private_ip_address]
+}
